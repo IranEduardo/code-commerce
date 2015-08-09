7bfff046f4a093d82f369f423e75dcdc42ca760a
Iran Eduardo <iranedu@hotmail.com>
2015-08-09 04:37:27 -0300

    Quinta Fase do Projeto: Seeding (Category, Product and User Tables)

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('categories')->truncate();
+        factory(CodeCommerce\Category::class,15)->create();
+    }
+
+}
---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('products')->truncate();
+
+        factory(CodeCommerce\Product::class,15)->create();
+    }
+
+
+}
