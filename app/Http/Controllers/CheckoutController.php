90d1ef01b37d60aaa193101f10c096cea7956820
Iran Eduardo <iranedu@hotmail.com>
2016-06-01 00:13:04 -0300

    Decima Quinta Fase do Projeto: Integração com o PagSeguro

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -10,11 +10,15 @@ use CodeCommerce\Order;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
+use PHPSC\PagSeguro\Items\Item;
+use PHPSC\PagSeguro\Requests\Checkout\CheckoutService;
+use PHPSC\PagSeguro\Requests\Checkout\Checkout;
+
 
 class CheckoutController extends Controller
 {
 
-    public function place(Order $orderModel)
+    public function place(Order $orderModel, CheckoutService $checkoutService)
     {
         if (!Session::has('cart'))
             return false;
@@ -23,9 +27,13 @@ class CheckoutController extends Controller
 
         if ($cart->getTotal() > 0){
 
+            $checkout = $checkoutService->createCheckoutBuilder();
+
             $order = $orderModel->create(['user_id' => Auth::user()->id, 'total' => $cart->getTotal()]);
 
             foreach($cart->all() as $k => $item) {
+
+                $checkout->addItem(new Item($k,$item['name'],number_format($item['price'],2,'.',''),$item['qtd']));
 
                 $order->items()->create(['product_id' => $k, 'price' => $item['price'], 'qtd' => $item['qtd']]);
 
@@ -33,7 +41,11 @@ class CheckoutController extends Controller
 
             $cart->clear();
 
-            return redirect()->route('account.orders');
+            $response = $checkoutService->checkout($checkout->getCheckout());
+
+            return redirect($response->getRedirectionUrl());
+
+            /*return redirect()->route('account.orders'); */
 
          }
 
